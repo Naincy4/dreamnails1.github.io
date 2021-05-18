68c13543b7d71e4ec1095243c571659d0623621f
Naincy4 <naincygupta410@gmail.com>
2021-05-18 12:39:28 +0530

    my website

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body onload="myFunction()">
+<div id="loading"></div>
+    <div class="header1">
+    <div class="header01">Signin/Join</div>
+        <div class="header02">Free Shiping Worldwide</div>
+        
+</div>
+<div class="header2">
+    <div class="header02">
+
+<img src="image/1.png" alt="logo">
+</div>
+</div>
+<nav class="menu">
+<ul>
+    <li><a href="#home">NEW</a></li>
+    <li><a href="#gallery">GALLERY</a></li>
+    <li><a href="#contact">BEAUTY</a></li>
+    <li><a href="#contact">VIDEO</a></li>
+    <li><a href="#contact">NAILS</a></li>
+    <li><a href="#contact">CONTACT</a></li>
+</ul>
+<form class="search-form">
+<input type="text" placeholder="search">
+<button> Search</button>
+</form>
+</nav>
+<div class="header3">
+
+    
+<p><i>Let</i><br> <spam><b>your nails do the</b></spam><br> talking</p>
+</div>
+<div class="header5">
+    <div class="header05"><h3><u>About </u><span><b><u><i>Our</i></u></b></span><u> Salon</u></h3>
+    <img src="image/3.jpg" alt="logo"><p>Dream nails invites you to experience the beauty of pampering nails. Make it a special occasion every time with our team of professionals. We take pride in the superb quality of our products and procedures so you could leave satisfied. With our nail treatments, we not only leave your nails looking dazzling but also make them feel rejuvenated.<br>
+
+We maintain the highest level of cleanliness and sterilization.Our services are not limited to nails only. We provide quality waxing and eyelashes extension services for a beautiful you. When it comes to hosting party and get-togethers, we get you covered.</p>
+</div>
+</div>
+<div class="gallery">
+    <h2><span><i><u>Our</u></i></span><u> Gallery</u></h2>
+    <img src="image/4.jpg" alt="logo">
+    <img src="image/5.jpg" alt="logo">
+    <img src="image/6.jpg" alt="logo">
+    <img src="image/7.jpg" alt="logo">
+    <img src="image/8.jpg" alt="logo">
+    <img src="image/9.jpg" alt="logo">
+    <img src="image/10.jpg" alt="logo">
+    <img src="image/11.jpg" alt="logo">
+    <img src="image/12.jpg" alt="logo">
+    
+</div>
+<div class="employ"><p><i><b>Employ our Coupons</b></i></p></div>
+<div class="services"><h2><span><i><u>Our </u></i></span><u>Services</u></h2>
+</div>
+<div class="slider1">
+
+<div class="sliderbox">
+<img src="image/14.jpg" alt="logo">
+    <img src="image/15.jpg" alt="logo">
+    <img src="image/17.jpg" alt="logo">
+    <img src="image/18.jpg" alt="logo">
+    
+    
+</div>
+<div class="sliderbox1">
+    <div class="sliderbox2"><h3>Nail Enhancement</h3><p>The curing light technology, already known from dental medicine, cling the new nails to the others without leaving a trace. The result is a completely new nail. We are going to show you how you could have beautiful hands and well-manicured nails.</p></div>
+    <div class="sliderbox2"><h3>Nail Art</h3><p>Nail art is a creative way to paint, decorate, enhance, and embellish the nails. It is a type of artwork that can be done on fingernails and toenails, usually after manicures or pedicures. A manicure and a pedicure are beauty treatments that trim, shape, and polish the nail.</p></div>
+    <div class="sliderbox2"><h3>Waxing</h3><p>Waxing is the process of hair removal from the root by using a covering of a sticky substance, such as wax, to adhere to body hair, and then removing this covering and pulling out the hair from the follicle. New hair will not grow back in the previously waxed area for four to six weeks.</p></div>
+    <div class="sliderbox2"><h3>Eyelash Extension</h3><p>Eyelash extensions are a cosmetic application used to enhance the length, curl, fullness, and thickness of natural eyelashes. The extensions may be made from several materials including mink, silk, synthetic, human or horsehair. The main method of applying the extensions is by using a cyanoacrylate adhesive.</p></div>
+</div>
+</div>
+<div class=button>
+    <input type="button" value="VIEW ALL">
+</div>
+<div class="contact"><h2><span><i><u>Contact</u></i></span><u> Us</u></h2><br>
+Name :  <input type="text" name="Name">
+Email :  <input type="email" name="Email">
+</div>
+<div class="bottom">
+    <div class="bottom1"><h3>Contact</h3><br>
+    <ul>
+    <li><img src="image/21.jpg" alt="logo">Boring Road, Patna-800009</li><br><br>
+    <li><img src="image/22.jpg" alt="logo">91-7906783468</li><br><br>
+    <li><img src="image/gmail.png" alt="logo">dreamnails100@gmail.com</li>
+</ul>
+    
+
+</p>
+</div>
+<div class="bottom2"><h3>We are open</h3><br>
+<p>Sunday    Closed<br><br>
+  Monday-Thursday  10:00-3:00<br><br>
+   Thursday-Friday  10:00-5:00<br><br>
+   Saturday   9:00-12:00</p>
+</div>
+<div class="bottom3"><h3>Social links</h3>
+<img src="image/facebook (1).png" alt="logo">
+<img src="image/twitter (1).png" alt="logo">
+<img src="image/instagram.png" alt="logo">
+<img src="image/pinterest.png" alt="logo">
+<img src="image/youtube.png" alt="logo">
+</div>
+</div>
+<div class="payment"><h3>Terms of Payment</h3></div>
+<div class="payment0">
+    <div class="payment1"></div>
+<div class="payment2">
+<img src="image/visa.png" alt="logo">
+<img src="image/paypal.png" alt="logo">
+<img src="image/mastercard.png" alt="logo">
+</div>
+
+</div>
+<div class="last"><p>Copyright @ 2020-2025 Dream Nails Powered by Amazing7.studios</p>
+</div>
+<script>
+    var preloader = document.getElementById('loading');
+    function myFunction(){
+        preloader.style.display='none';
+    }
+</script>
+
+</body>
+</html>
